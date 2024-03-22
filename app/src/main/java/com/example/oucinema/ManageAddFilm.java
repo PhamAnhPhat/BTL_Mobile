fae71b875a255cb5466e814b0fbd2c2900ef98a1
PhamAnhPhat-2051050342 <haichao000@gmail.com>
2024-03-22 16:14:59 +0700

    22/3/2024

--- app/src/main/java/com/example/oucinema/ManageAddFilm.java
+++ app/src/main/java/com/example/oucinema/ManageAddFilm.java
@@ -4,26 +4,39 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-//import com.cloudinary.android.* ;
 import com.example.oucinema.model.Phim;
-
+import android.content.ContentResolver;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import android.content.res.Resources;
+import android.content.Context;
 
 public class ManageAddFilm extends AppCompatActivity {
     DBHelper dbHelper;
@@ -32,6 +45,7 @@ public class ManageAddFilm extends AppCompatActivity {
     ImageView e ;
     private static String urlString;
     private static final int PICK_IMAGE_REQUEST_CODE = 1;
+    private static final int REQUEST_CODE_PERMISSIONS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,15 +103,22 @@ public class ManageAddFilm extends AppCompatActivity {
             btnXoaFilm.setTextColor(Color.parseColor("#C7C8CC"));
 
         } else {
+            String[] permissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_PERMISSIONS);
+
             etTenPhim.setText(itemName);
             etMoTa.setText(itemMoTa);
             etTheLoai.setText(itemTheLoai);
             etThoiLuong.setText(String.valueOf(itemThoiLuong));
             etNgayPhatHanh.setText(itemNgayPhatHanh);
             etDaoDien.setText(itemDaoDien);
+
+            File file = new File(getFilesDir(), itemHinhAnh);
+            Drawable drawable = Drawable.createFromPath(file.getAbsolutePath());
+            ImageView imageView = findViewById(R.id.imageView13);
+            imageView.setImageDrawable(drawable);
+
             Log.d("test url","test "+itemHinhAnh);
-            Log.d("test url","testurl "+Uri.parse(itemHinhAnh));
-            e.setImageURI(Uri.parse(itemHinhAnh));
 
 
         }
@@ -129,6 +150,7 @@ public class ManageAddFilm extends AppCompatActivity {
                     Toast.makeText(ManageAddFilm.this, "Thêm phim Thất bại", Toast.LENGTH_LONG).show();
                 }
                 }catch (Exception e){
+                    Log.d("log exception" , "log :" +e);
                     Toast.makeText(ManageAddFilm.this, "Chưa chọn hình ảnh", Toast.LENGTH_LONG).show();
                 }
 
@@ -166,6 +188,7 @@ public class ManageAddFilm extends AppCompatActivity {
                     java.sql.Date date = java.sql.Date.valueOf(etNgayPhatHanh.getText().toString());
                     phim.setNgayPhatHanh(date);
                     phim.setDaoDien(etDaoDien.getText().toString());
+                    phim.setHinhAnh(urlString);
                     phim.setLinkTrailer(etLinkTrailer.getText().toString());
                     String idphim = String.valueOf(itemId);
                     phim.setUserUpdate(Integer.parseInt(user_id));
@@ -194,16 +217,16 @@ public class ManageAddFilm extends AppCompatActivity {
         btnHinhAnh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-//                Log.d("hinh anh",urlString);
-                startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE);
+                pickImage();
             }
         });
 
     }
-
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -211,10 +234,33 @@ public class ManageAddFilm extends AppCompatActivity {
 
         if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             Uri imageUri = data.getData();
-            e = findViewById(R.id.imageView13);
-            e.setImageURI(imageUri);
-            urlString = String.valueOf(imageUri);
-            Log.d("d ",String.valueOf(imageUri));
+
+            if (imageUri != null) {
+                try {
+                    // Lấy bitmap từ URI ảnh
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+
+                    // Tạo tên file PNG
+                    String fileName = "image_" + etTenPhim.getText().toString() + ".png";
+
+                    // Lưu bitmap vào file PNG
+                    File file = new File(getFilesDir(), fileName);
+                    FileOutputStream out = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                    out.close();
+
+                    // Thêm file PNG vào drawable
+                    int drawableName = getResources().getIdentifier(fileName, "drawable", getPackageName());
+                    Drawable drawable = Drawable.createFromPath(file.getAbsolutePath());
+                    ImageView imageView = findViewById(R.id.imageView13);
+                    imageView.setImageDrawable(drawable);
+
+                    urlString = fileName;
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
